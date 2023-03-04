0150d8869538376d52fb227e3c9640c88e23db77
Parul Chaddha <106451107+parulchaddha@users.noreply.github.com>
2023-03-04 20:51:47 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import Navbar from './components/Navbar';
-/*<BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter> */
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Home from './components/home';
+import Donor from './components/donor/donor';
+
 function App() {
   return (
    <>
-    <Navbar/>
+   <BrowserRouter>
+   <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/donor" element={<Donor/>}/>
+        
+        </Routes>
+    </BrowserRouter> 
+   
    </> 
   );
 }
