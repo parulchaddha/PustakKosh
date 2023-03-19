37814bb97f7444232c398248cdb3fe132f7bf994
Rupika <rupikasinha23@gmail.com>
2023-03-19 23:51:59 +0530

    Added display all function

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,12 @@ from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']='10082003'
-app.config['MYSQL_DB']='pustakkosh'
-mysql=MySQL(app)
-bcrypt=Bcrypt(app)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '10082003'
+app.config['MYSQL_DB'] ='pustakkosh'
+mysql = MySQL(app)
+bcrypt = Bcrypt(app)
 
 # cur=mysql.connection.cursor()
 app.secret_key="super secret key"
