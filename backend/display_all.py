326a7cda650022aba2ed25eebd46c8471e5c7cfa
Rupika <rupikasinha23@gmail.com>
2023-03-20 00:10:18 +0530

    Fixed naming errors in previous commit

--- backend/display_all.py
+++ backend/display_all.py
@@ -2,16 +2,16 @@ from flask import Flask,render_template,request,redirect,url_for,session,jsonify
 from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 
-app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '10082003'
-app.config['MYSQL_DB'] ='pustakkosh'
-mysql = MySQL(app)
-bcrypt = Bcrypt(app)
+display_all = Flask(__name__)
+display_all.config['MYSQL_HOST'] = 'localhost'
+display_all.config['MYSQL_USER'] = 'root'
+display_all.config['MYSQL_PASSWORD'] = '10082003'
+display_all.config['MYSQL_DB'] ='pustakkosh'
+mysql = MySQL(display_all)
+bcrypt = Bcrypt(display_all)
 
-app.secret_key="super secret key"
-@app.route('/display_all',methods={'GET','POST'})
+display_all.secret_key="super secret key"
+@display_all.route('/',methods={'GET','POST'})
 def display_books():
     cur = mysql.connection.cursor()
     query = "SELECT * FROM book"
@@ -21,3 +21,6 @@ def display_books():
     for book in books:
         print(book)
     cur.close()
+    
+if __name__=='main':
+    display_all.run(debug=True)
