aa251208a8d20edc4603cba16e49174a0efe8b62
PrachiK003 <98697768+PrachiK003@users.noreply.github.com>
2023-03-24 19:06:48 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,29 @@
 from flask import Flask,render_template,request,redirect,url_for,session,jsonify
-import db
+import db_service
 from flask_mysqldb import MySQL
 #from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-#app.config['MYSQL_HOST']='localhost'
-#app.config['MYSQL_USER']='root'
-#app.config['MYSQL_PASSWORD']='Sonam11'
-#app.config['MYSQL_DB']='pustakkosh'
-#mysql=MySQL(app)
-#bcrypt=Bcrypt(app)
+
 
 
 
 @app.route('/get_all_books',methods={'GET'})
 def get_all_books():
-
-    # get query params
-    # how to get get parameter and post parameter from flast request object
+    """ 
+    to fetch 
+    """
+    
     user_id = request.args.get('user_id')
     donation_status = request.args.get('donation_status')
-    # print(user_id,donation_status)
-    result = db.get_all_books(user_id, donation_status)
-    # result={"user_id":user_id}
+   
+    result = db_service.get_all_books(user_id, donation_status)
+   
     return jsonify(result)
 
 
 
-# add_new_book(user_id, book_name, author, genre, description, status)
+
 @app.route('/add_new_book',methods={'POST'})
 def add_new_book():
 #    if request.method=='POST':
@@ -40,7 +36,7 @@ def add_new_book():
     description=data['description']
 
     donation_status = data['donation_status']
-    result=db.add_new_book(user_id, book_name, author, genre, description, donation_status)
+    result=db_service.add_new_book(user_id, book_name, author, genre, description, donation_status)
     return jsonify(result)
 
 @app.route('/update_request_for_book',methods={'POST'})
@@ -53,7 +49,7 @@ def update_request_for_book():
     
 
     
-    result=db.update_request_for_book(book_id,request_user_id)
+    result=db_service.update_request_for_book(book_id,request_user_id)
     return jsonify(result)
 
 
@@ -65,84 +61,35 @@ def get_requested_items():
     request_user_id = request.args.get('user_id')
     
     # print(user_id,donation_status)
-    result = db.get_requested_items(request_user_id)
+    result = db_service.get_requested_items(request_user_id)
     # result={"user_id":user_id}
     return jsonify(result)
 
-app.secret_key="super secret key"
-@app.route('/register',methods={'GET','POST'})
-def index():
+@app.route('/get_needy_info',methods={'GET'})
+def get_needy_info():
+
+    # get query params
+    # how to get get parameter and post parameter from flast request object
+    book_id = request.args.get('book_id')
+    
+    # print(user_id,donation_status)
+    result = db_service.get_needy_info(book_id)
+    print(result)
+    # result={"user_id":user_id}
+    return jsonify(result)
+@app.route('/accept_book_request',methods={'POST'})
+def accept_book_request():
+#    if request.method=='POST':
+   
+    data=request.get_json()
+    request_id = data['request_id']
+    book_id=data['book_id']
+    
 
     
-    if request.method=='POST':
-        userdetails=request.form
-        name=userdetails['name']
-        password=userdetails['password']
-        location=userdetails['location']
-        cur=mysql.connection.cursor()
-        cur.execute("INSERT INTO user(user_name,user_pass,location) VALUES(%s,%s,%s)",(name,password,location))
-        
-        mysql.connection.commit()
-        cur.close()
-        return redirect(url_for('login'))
-    return render_template('index.html')
-@app.route('/login',methods={'GET','POST'})
+    result=db_service.accept_book_request(book_id,request_id)
+    return jsonify(result)
 
-def login():
-
-    
-    if request.method=='POST':
-        
-        userdetails=request.form
-        name=userdetails['name']
-        password=userdetails['password']
-        print(name,password)
-        cur=mysql.connection.cursor()
-        cur.execute('SELECT *FROM user WHERE user_name=%s AND user_pass=%s',(name,password))
-        record=cur.fetchone()
-        print(record)
-        if record:
-            print('1')
-            session['loggedin']=True
-            session['user_id']=record[0]
-            return redirect(url_for('book'))
-        else:
-            msg="INCOREECT DETAILS"
-     
-       
-    return render_template('login.html')
-
-@app.route('/book',methods={'GET','POST'})
-def book():
-    if request.method=='POST':
-        book=request.form
-        name=book['name']
-        author=book['author']
-        genre=book['genre']
-        user_id=session['user_id']
-        cur=mysql.connection.cursor()
-        cur.execute("INSERT INTO book(book_name,author,genre,user_id) VALUES(%s,%s,%s,%s)",(name,author,genre,user_id))
-        mysql.connection.commit()
-        cur.close()
-        return 'book_added'
-    return render_template('book.html',user_id=session['user_id'])
-# @app.route('/test',methods={'GET','POST'})
-# def test():
-#    d={
-#        '1':'book_name','2':'author'
-#    }
-#    return jsonify(d)
-
-@app.route('/needy_dis',methods={'GET','POST'})
-def needy_dis():
-    cur = mysql.connection.cursor()
-    query = "SELECT * FROM book"
-    cur.execute(query)
-    books = cur.fetchall()
-    print("All Available Books: ")
-    for book in books:
-        print(book)
-    cur.close()
 
        
 if __name__=='__main__':
